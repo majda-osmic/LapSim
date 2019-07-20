8bcc8a4cf957408f86e1c3c5fbd010decde59056
Majda Osmic <mobearette@gmail.com>
2019-07-20 07:21:23 +0200

    Fix simulation component unit test

--- src/app/simulation/simulation.component.spec.ts
+++ src/app/simulation/simulation.component.spec.ts
@@ -2,10 +2,21 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SimulationComponent } from './simulation.component';
+import { ISimulation } from '../interfaces';
 
 describe('SimulationComponent', () => {
   let component: SimulationComponent;
   let fixture: ComponentFixture<SimulationComponent>;
+  const mockSimulation: ISimulation =  {
+    id:  1,
+    startTime: new Date(2019, 1, 6, 12, 38, 45),
+    endTime: new Date(2019, 1, 6, 13, 56, 21),
+    name: 'Sim 1',
+    cpus: 25,
+    usedBudget: 58,
+    runs: 400,
+    location: 'West US'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +29,7 @@ describe('SimulationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SimulationComponent);
     component = fixture.componentInstance;
+    component.simulation = mockSimulation;
     fixture.detectChanges();
   });
 

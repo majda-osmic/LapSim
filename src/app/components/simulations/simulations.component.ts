1a5aa4e8f2628d8a764e8af7351c3cfbc23954f9
Majda Osmic <mobearette@gmail.com>
2019-07-27 13:02:45 +0200

    Show progress bar in sim view

--- src/app/components/simulations/simulations.component.ts
+++ src/app/components/simulations/simulations.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ISimulation } from 'src/app/data-interfaces';
+import { ISimulation, ITeam } from 'src/app/data-interfaces';
 import { SimulationsService } from 'src/app/services/simulations.service';
 import { ActivatedRoute } from '@angular/router';
+import { TeamsService } from 'src/app/services/teams.service';
 
 @Component({
   selector: 'app-simulations',
@@ -9,22 +10,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./simulations.component.scss'],
 })
 export class SimulationsComponent implements OnInit {
-  teamID: number;
+  team: ITeam;
+  progress: number;
   simulations: ISimulation[] = [];
 
   constructor(private simService: SimulationsService,
+              private teamService: TeamsService,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.teamID = +this.route.snapshot.params.id;
-    this.simulations =  this.simService.getVisibleSimulationsForTeam(this.teamID);
+    const teamID = +this.route.snapshot.params.id;
+    this.team = this.teamService.getTeam(teamID);
+    this.progress = this.team.usedBudget / this.team.budget;
+    this.simulations = this.simService.getVisibleSimulationsForTeam(teamID);
     this.simService.visibleItemsChanged.subscribe(teamId => this.updateVisibleSimulations(teamId));
-    this.updateVisibleSimulations(this.teamID);
+    this.updateVisibleSimulations(teamID);
   }
 
   private updateVisibleSimulations(id: number) {
-    if (this.teamID === id) {
-      this.simulations =  this.simService.getVisibleSimulationsForTeam(id);
+    if (this.team.id === id) {
+      this.simulations = this.simService.getVisibleSimulationsForTeam(id);
     }
   }
 }
